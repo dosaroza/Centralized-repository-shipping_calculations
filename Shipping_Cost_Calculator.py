52db61c0f9cc855260f1f635cb1612ab62c9efbd
Domingo Del Rosario <dosaroza@gmail.com>
2025-08-13 11:44:18 -0400

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -8,5 +8,4 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 shipping_cost = weight * rate
 
 ## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
- # Aquí hay una nueva actualización por <dosaroza>
+print(f"Shipping Cost: {shipping_cost} USD
